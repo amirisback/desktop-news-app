e46640d2ac4be548a639a28cab703ff1028da986
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-09-09 23:02:17 +0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.21"
+    id("org.jetbrains.compose") version "1.0.0-alpha3"
+}
+
+group = "com.frogobox"
+version = "1.0"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven { url = uri("https://jitpack.io") }
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation("io.ktor:ktor-client-core:1.6.3")
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "CaravanDesktopCompose"
+            packageVersion = "1.0.0"
+        }
+    }
+}
